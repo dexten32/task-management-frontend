75ddde620a2cee35a70f9e29e85a28178e394af1
harsh <harshbajaj544@gmail.com>
2025-07-29 10:53:53 +0530

    Landing Page issue fix

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-gray-800 text-white">
       {/* Pass the login handler to the landing page */}
-      <ServiceCompanyLanding onLogin={handleLogin} />
+      <ServiceCompanyLanding />
     </div>
   );
 }
